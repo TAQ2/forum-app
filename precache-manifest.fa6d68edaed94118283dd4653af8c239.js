251e46ccd154e501cb68e08bff3ba0fdef207942
Arjun <arjungupta2110@gmail.com>
2020-01-22 18:40:42 +0000

    Updates

---
+++ precache-manifest.fa6d68edaed94118283dd4653af8c239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e026572db2db6742e68a783b8763360",
+    "url": "/forum-app/index.html"
+  },
+  {
+    "revision": "57d1daf6a8213acf7b0e",
+    "url": "/forum-app/static/css/main.9e662c90.chunk.css"
+  },
+  {
+    "revision": "a60a2bc7ff20526036e7",
+    "url": "/forum-app/static/js/2.ebff9357.chunk.js"
+  },
+  {
+    "revision": "57d1daf6a8213acf7b0e",
+    "url": "/forum-app/static/js/main.4b29424b.chunk.js"
+  },
+  {
+    "revision": "0c96824797fa11b703da",
+    "url": "/forum-app/static/js/runtime~main.22f28b54.js"
+  },
+  {
+    "revision": "296a405865940fff0b995f1487044fb8",
+    "url": "/forum-app/static/media/Chomsky.296a4058.otf"
+  }
+]);
