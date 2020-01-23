4d72faf6a534f57b52f16ba5c1b766f8cde131a0
Arjun <arjungupta2110@gmail.com>
2020-01-23 15:02:53 +0000

    remove unused css

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import "react-toastify/dist/ReactToastify.css";
 
 import Header from "../Header";
 import Toaster from "../../components/Toaster";
